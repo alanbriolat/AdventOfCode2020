94a7a609de9a9f63c5645e4fe4bed792de63e923
Alan Briolat <alan.briolat@gmail.com>
2020-12-04 08:37:42 +0000

    meta: add timing and filtering of solutions

--- src/bin/aoc2020.rs
+++ src/bin/aoc2020.rs
@@ -1,9 +1,23 @@
+use std::env;
+use std::time;
+
 use aoc2020::solutions;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
     let runner = solutions::build_runner();
-    runner.run_all().for_each(|(name, result)| match result {
-        Ok(output) => println!("{}: {}", name, output),
-        Err(err) => println!("ERROR: {}: {}", name, err),
-    });
+    let names: Vec<_> = match args.get(1) {
+        Some(filter) => runner.list().filter(|name| name.contains(filter)).collect(),
+        None => runner.list().collect(),
+    };
+
+    for name in names.iter() {
+        let start = time::Instant::now();
+        let result = runner.run(name);
+        let elapsed = time::Instant::now().duration_since(start);
+        match result {
+            Ok(output) => println!("{}: {} ({:?})", name, output, elapsed),
+            Err(err) => println!("ERROR: {}: {}", name, err),
+        }
+    }
 }
